e1a7504b43408319ede3b9e36ba53784b23fb286
vonshick <kubawasik@vp.pl>
2018-12-27 16:14:34 +0100

    Client connects - calendar reacts

--- src/Client.java
+++ src/Client.java
@@ -1,49 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
+
 
 public class Client extends Thread{
+    Socket clientSocket;
+    BufferedReader reader;
+    PrintWriter writer;
+    String host;
+    int port;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
     public void run() {
-        Socket clientSocket = null;
+//        Socket clientSocket = null;
         try {
-            clientSocket = new Socket("localhost", 1234);
+//            clientSocket = new Socket("localhost", 1234);
+            clientSocket = new Socket(host, port);
+//            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            writer = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(clientSocket.getOutputStream(), true);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         while(true) {
-            BufferedReader reader = null;
-            try {
-                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String serverMessage = null;
-            try {
-                serverMessage = reader.readLine();
-                if (serverMessage == null){
-                    clientSocket.close();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj odpowiednia liczbe:\n- przesyl: 1\n- odczyt: 2");
+            String choice = scanner.nextLine();
+            if (choice.equals("1")){
+                System.out.println("Podaj wiadomosc do przeslania:\n");
+                String string = scanner.nextLine();
+                writer.println(string);
+            } else if( choice.equals("2")){
+                String serverMessage = null;
+                try {
+                    serverMessage = reader.readLine();
+                    if (serverMessage == null){
+                        clientSocket.close();
+                        break;
+                    }
+                    else {
+                        System.out.println(serverMessage);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                else {
-                    System.out.println(serverMessage);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                System.out.println("Podałeś złą liczbę - podaj ją jeszcze raz.\n");
             }
-
-    // ###############	II task  ###############
-    //            InputStream inputStream = clientSocket.getInputStream();
-    //            byte[] buffer = new byte[100];
-    //            int bytesCount = inputStream.read(buffer);
-    //            if ( bytesCount == -1) {
-    //                clientSocket.close();
-    //            }
-    //            else {
-    //                System.out.write(buffer, 0, bytesCount);
-    //            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String args[]) {
-        new Client().start();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Pass server IP address:\n");
+//        String host = scanner.nextLine();
+//        System.out.println("Pass server port:\n");
+//        int port = Integer.parseInt(scanner.nextLine());
+//        Client client = new Client(host, port);
+//        client.start();
+        //        new Client().start();
         new CalendarInterface().start();
     }
 }
